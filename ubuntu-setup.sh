04d17b65e5c2db3a727b94beae6ed10965468ddc
Josh-XT <102809327+Josh-XT@users.noreply.github.com>
2022-05-15 22:28:39 -0400

    Add files via upload

---
+++ ubuntu-setup.sh
@@ -0,0 +1,8 @@
+sudo apt update && sudo apt upgrade -y
+sudo apt install git nodejs npm evolution evolution-ews gnome-boxes -y
+sudo npm install --global yarn
+sudo snap install --classic powershell
+sudo snap install --classic code
+sudo snap install onlyoffice-desktopeditors
+sudo snap install --edge discord
+sudo snap install --edge spotify
