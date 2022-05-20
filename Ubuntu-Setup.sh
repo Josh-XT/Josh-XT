e71eaae0399a5c5416eb9fc904b9d07253713f0b
Josh XT <josh@devxt.com>
2022-05-20 07:35:13 -0400

    Updated packages

--- Ubuntu-Setup.sh
+++ Ubuntu-Setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 echo "alias update='sudo apt update && sudo apt upgrade -y'" > eval echo "~$different_user/\.bash_aliases"
+wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
 sudo apt update && sudo apt upgrade -y
-sudo apt install git nodejs npm evolution evolution-ews gnome-boxes -y
+sudo apt install git nodejs npm evolution evolution-ews gnome-boxes apt-transport-https dotnet-sdk-6.0 aspnetcore-runtime-6.0 -y
 sudo npm install --global yarn
 sudo snap install --classic powershell
 sudo snap install --classic code
