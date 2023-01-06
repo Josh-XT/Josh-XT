35e655245c0ab1c78ffd6600c0935b82f3104d6e
Josh XT <josh@devxt.com>
2023-01-06 14:49:32 -0500

    Updates

--- DebSetup.sh
+++ DebSetup.sh
@@ -3,7 +3,8 @@ sudo echo "alias update='sudo apt update && sudo apt upgrade -y'" > "$HOME/.bash
 wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
-curl -sL https://deb.nodesource.com/setup_16.x | sudo bash -
+curl -sL https://deb.nodesource.com/setup_18.x | sudo bash -
+sudo apt install -y nodejs
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 rm google-chrome-stable_current_amd64.deb
@@ -13,23 +14,22 @@ rm libssl1.1_1.1.1f-1ubuntu2_amd64.deb
 sudo apt update && sudo apt upgrade -y
 sudo apt remove -y libreoffice*
 sudo apt install -y python3
-sudo apt install -y pip
+sudo apt install -y python3-pip
 sudo apt install -y libpq-dev
 sudo apt install -y docker-compose
 sudo apt install -y snapd
-sudo apt install -y nodejs
 sudo apt install -y evolution 
 sudo apt install -y evolution-ews 
 sudo apt install -y gnome-boxes 
 sudo apt install -y apt-transport-https 
 sudo apt install -y dotnet-sdk-6.0 
 sudo apt install -y aspnetcore-runtime-6.0
-sudo apt install -y powershell
 sudo apt install -y code
 sudo apt install -y piper
-sudo /usr/local/bin/python -m pip install --upgrade pip
+pip install --upgrade pip
 sudo apt autoremove -y
 sudo npm install --global yarn
+sudo snap install powershell --classic
 sudo snap install -y discord
 sudo snap install onlyoffice-desktopeditors
 sudo snap install --edge spotify
