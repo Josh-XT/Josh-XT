635852e0c87de41c0579bf194915a245d8b008bb
Josh XT <josh@devxt.com>
2023-01-09 14:17:15 -0500

    Updates

--- DebSetup.sh
+++ DebSetup.sh
@@ -33,14 +33,8 @@ sudo snap install -y discord
 sudo snap install onlyoffice-desktopeditors
 sudo snap install --edge spotify
 sudo snap refresh
-sudo pip install jupyter
-sudo pip install powershell_kernel
-sudo python3 -m powershell_kernel.install
-sudo python3 -m powershell_kernel.install --powershell-command pwsh
-pip install jupyter
-pip install powershell_kernel
-python3 -m powershell_kernel.install
-python3 -m powershell_kernel.install --powershell-command pwsh
+sudo pip install jupyter && pip install jupyter
+sudo pip install matplotlib && pip install matplotlib
 git config --global submodule.recurse true
 git config --global user.email "josh@devxt.com"
 git config --global user.name "Josh XT"
